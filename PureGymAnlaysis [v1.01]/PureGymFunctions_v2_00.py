12f9ce920abb23fe272283ad3cb7e5fd9caa83fe
Chris <75547755+chrisjackr@users.noreply.github.com>
2021-01-10 12:22:45 +0000

    Update and rename PureGymFunctions_v1_01.py to PureGymFunctions_v2_00.py

--- PureGymAnlaysis [v1.01]/PureGymFunctions_v2_00.py
+++ PureGymAnlaysis [v1.01]/PureGymFunctions_v2_00.py
@@ -1,4 +1,5 @@
 #PureGymFunctions
+import datetime
 
 def time2num(t):
 #Takes a (date)time and converts it to a number of minutes index
@@ -10,6 +11,6 @@ def time2num(t):
 def num2time(n):
 #takes a number of minutes index and converts it to a (date)time
     minute = int(n%60)
-    hour = int((n-m)/60)
-    t = datetime.datetime(2020,11,19,h,m,0)
+    hour = int((n-minute)/60)
+    t = datetime.datetime(2020,1,1,hour,minute,0,0)
     return t, hour, minute
